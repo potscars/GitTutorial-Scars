0a651b9112516100a57dcb800afb3fc65c4fec61
Hainizam <Ingeniworks@hainizams-mac-mini.local>
2017-09-07 12:26:13 +0800

    Initial Commit

---
+++ GitTutorial-Scars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial-Scars
+//
+//  Created by Hainizam on 07/09/2017.
+//  Copyright © 2017 com.ingeniworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
